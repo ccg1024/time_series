8fd167e8bbb217546b740996abf1165f4657d508
ccg <aimerchenyuan@163.com>
2022-07-27 17:01:17 +0800

    finish test part

--- train/dataload_new2.py
+++ train/dataload_new2.py
@@ -44,13 +44,14 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print("current training device: ", device)
 
 s2s = seq2seq.Seq2Seq(12, 50, 1, 1, 1,layer_num=1, device=device)
-epochs = 10
+epochs = 100
 loss_fn = nn.MSELoss()
 optimizer = optim.Adam(s2s.parameters(), lr=0.001)
 
 last_train = []
 for epoch in range(epochs):
     hn = cn = None
+    train_error = []
     print("training epoch: ", epoch)
     for i in range(test_split - 1):
         input_window = train_dat[i:i+1]
@@ -81,7 +82,9 @@ for epoch in range(epochs):
         if epoch == epochs - 1:
             last_train.append(prediction.item())
 
-        print(loss.item())
+        # print(loss.item())
+        train_error.append(loss.item())
+    print("average loss: ", (np.asarray(train_error) / len(train_error)).sum())
 
 
 last_train = np.asarray(last_train)
@@ -102,4 +105,45 @@ plt.legend()
 plt.savefig("../tests/renom_predict.png")
 
 # test
+test_input_data = train_dat[-1:]
+test_input_data = np.concatenate((test_input_data, test_dat[0:]), axis=0)
+test_len = len(test_input_data)
+test_cord = []
+test_error = []
+s2s.eval()
+for i in range(test_len-1):
+    input_window = test_input_data[i:i+1]
+    target_window = test_input_data[i+1:i+2]
+
+    encoder_input = input_window[:, 1:]
+    decoder_input = input_window[:, 0]
+
+    decoder_input = normalization(decoder_input, min_val, max_val)
+
+    encoder_input = torch.Tensor(encoder_input).reshape((1, 1, 12)).to(device)
+
+    decoder_input = torch.Tensor(decoder_input).reshape((1, 1, 1)).to(device)
+
+    target_val = target_window[:, 0]
+    target_val = normalization(target_val, min_val, max_val)
+    target_val = torch.Tensor(target_val).reshape((1, 1))
+
+    with torch.no_grad():
+        prediction, hn, cn = s2s(encoder_input, decoder_input, hn, cn)
+        loss = loss_fn(prediction.to("cpu"), target_val)
+        test_cord.append(prediction.item())
+        test_error.append(loss.item())
+
+print("average test error: ", (np.asarray(test_error) / len(test_error)).sum())
+
+
+test_cord = np.asarray(test_cord)
+future_cord = np.asarray(test_dat[:, 0])
+test_cord = renormalization(test_cord, min_val, max_val)
+
+plt.close()
+plt.plot(np.arange(len(test_cord)), test_cord, color='black', label="predict")
+plt.plot(np.arange(len(future_cord)), future_cord, color='r', label='true')
+plt.legend()
+plt.savefig("../tests/renom_test.png")
 
