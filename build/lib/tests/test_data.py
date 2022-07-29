e2edfa1781e977b2463ea474ae042e55970e2c24
ccg <aimerchenyuan@163.com>
2022-07-29 20:19:15 +0800

    delete file about setup.py

--- build/lib/tests/test_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-pd_file = pd.read_csv("~/CodePlace/Python/pytorch/datasets/seaborn-data/master/flights.csv")
-one_hot_dat = pd.get_dummies(pd_file.iloc[:, 1:])
-
-gap = 6
-total_len = len(one_hot_dat)
-
--- build/lib/tests/test_lstm.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append("/home/william/CodePlace/Python/pytorch/time_series")
-from model import lstm
-
-print("hello word")
-test_obj = lstm.LSTM(1, 2)
-
